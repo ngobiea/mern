7891c1a6dada594d65b896bded0298f50b5678e7
ngobiea <ngobiea@gmail.com>
2024-04-04 11:16:37 +0500

    vercel

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
